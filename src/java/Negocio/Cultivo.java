2a1d342dd3ba3fca02baaa0fd863e2770d90e3bb
Luiz Antonio <luizantonioblr@gmail.com>
2014-07-02 17:30:56 -0300

    Alterações do cultivo para suporte a BD.

--- src/java/Negocio/Cultivo.java
+++ src/java/Negocio/Cultivo.java
@@ -2,7 +2,6 @@ package Negocio;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -11,8 +10,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @ManagedBean(name = "cultivo")
 @RequestScoped
@@ -25,9 +22,6 @@ public class Cultivo implements Serializable {
 
     @Column(name = "Nome", length = 25)
     private String nome;
-
-    @Temporal(TemporalType.DATE)
-    private Date dataInicio;
 
     @Column(name = "Area", length = 10)
     private double area;
@@ -59,14 +53,6 @@ public class Cultivo implements Serializable {
 
     public void setNome(String cultura) {
         this.nome = cultura;
-    }
-
-    public Date getDataInicio() {
-        return dataInicio;
-    }
-
-    public void setDataInicio(Date dataInicio) {
-        this.dataInicio = dataInicio;
     }
 
     public double getArea() {
